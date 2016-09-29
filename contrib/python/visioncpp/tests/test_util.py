9f14fd3dde57a227de3ac972b072e0c969d54235
Chris Cummins <chrisc.101@gmail.com>
2016-09-29 16:41:09 +0100

    Add util tests

---
+++ contrib/python/visioncpp/tests/test_util.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+from unittest import TestCase
+
+import visioncpp as vp
+from visioncpp import util
+
+
+class TestClass(object):
+	x = 5
+	def __init__(self, y):
+		self.y = y
+
+	def gety(self):
+		return self.y
+
+	def getxplus(self, z):
+		return self.x + z
+
+
+class test_util(TestCase):
+    def test_foreach(self):
+    	res = []
+    	def f(x):
+    		res.append(x)
+
+    	util.foreach(f,  [1, 2, 3, 4, 5])
+    	self.assertEqual([1, 2, 3, 4, 5], res)
+
+    def test_call_if_attribute(self):
+    	x = TestClass(10)
+    	self.assertEqual(util.call_if_attribute(x, "gety"), 10)
+    	self.assertEqual(util.call_if_attribute(x, "getxplus", 2), 7)
+    	self.assertEqual(util.call_if_attribute(x, "nope"), None)
+
+    def test_get_attribute(self):
+    	x = TestClass(10)
+    	self.assertEqual(util.get_attribute(x, "x"), 5)
+    	self.assertEqual(util.get_attribute(x, "nope"), None)
