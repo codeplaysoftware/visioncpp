867b94041f7bf345dab6cfdc2cb9965251086738
Chris Cummins <chrisc.101@gmail.com>
2016-11-29 15:17:04 +0000

    python: Update codegen test outputs

--- contrib/python/visioncpp/tests/test_codegen.py
+++ contrib/python/visioncpp/tests/test_codegen.py
@@ -16,35 +16,28 @@ class test_codegen(TestCase):
         node_in = vp.Image("examples/lena.jpg")
         node_out = vp.show(node_in)
         self.assertEqual(codegen.generate(node_out, "cpu",
-                                          use_clang_format=False),
-"""#include <opencv2/opencv.hpp>
+                                          use_clang_format=False)[1],
+"""\
 #include <visioncpp.hpp>
 
 extern "C" {
 
-int native_expression_tree() {
+int native_expression_tree(unsigned char *const in1, unsigned char *const out) {
 auto device = visioncpp::make_device<visioncpp::backend::sycl, visioncpp::device::cpu>();
 
 // inputs:
 std::shared_ptr<unsigned char> Image_1_data(new unsigned char[786432], [](unsigned char *d) { delete[] d; });
-cv::Mat show_1_cv(512, 512, CV_8UC(3), Image_1_data.get());
 
 {  // compute scope
-cv::Mat Image_1_cv = cv::imread("examples/lena.jpg");
-if (!Image_1_cv.data) {
-std::cerr << "Could not open or find the image examples/lena.jpg" << std::endl;
-return 1;
-}
-auto Image_1 = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(Image_1_cv.data);
+auto Image_1 = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(in1);
 auto Image_1_out = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(Image_1_data.get());
 auto show_1 = visioncpp::assign(Image_1_out, Image_1);
 visioncpp::execute<visioncpp::policy::Fuse, 16, 16, 8, 8>(show_1, device);
 }  // compute scope
 
 // outputs:
-cv::namedWindow("show_1", cv::WINDOW_AUTOSIZE);
-cv::imshow("show_1", show_1_cv);
-cv::waitKey(0);
+memcpy(out, Image_1_data.get(), 786432);
+  return 0;
 }
 
 }  // extern "C"
@@ -54,35 +47,28 @@ cv::waitKey(0);
         node_in = vp.Image("examples/lena.jpg")
         node_out = vp.show(node_in)
         self.assertEqual(codegen.generate(node_out, "gpu",
-                                          use_clang_format=False),
-"""#include <opencv2/opencv.hpp>
+                                          use_clang_format=False)[1],
+"""\
 #include <visioncpp.hpp>
 
 extern "C" {
 
-int native_expression_tree() {
+int native_expression_tree(unsigned char *const in1, unsigned char *const out) {
 auto device = visioncpp::make_device<visioncpp::backend::sycl, visioncpp::device::gpu>();
 
 // inputs:
 std::shared_ptr<unsigned char> Image_1_data(new unsigned char[786432], [](unsigned char *d) { delete[] d; });
-cv::Mat show_1_cv(512, 512, CV_8UC(3), Image_1_data.get());
 
 {  // compute scope
-cv::Mat Image_1_cv = cv::imread("examples/lena.jpg");
-if (!Image_1_cv.data) {
-std::cerr << "Could not open or find the image examples/lena.jpg" << std::endl;
-return 1;
-}
-auto Image_1 = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(Image_1_cv.data);
+auto Image_1 = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(in1);
 auto Image_1_out = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(Image_1_data.get());
 auto show_1 = visioncpp::assign(Image_1_out, Image_1);
 visioncpp::execute<visioncpp::policy::Fuse, 16, 16, 8, 8>(show_1, device);
 }  // compute scope
 
 // outputs:
-cv::namedWindow("show_1", cv::WINDOW_AUTOSIZE);
-cv::imshow("show_1", show_1_cv);
-cv::waitKey(0);
+memcpy(out, Image_1_data.get(), 786432);
+  return 0;
 }
 
 }  // extern "C"
