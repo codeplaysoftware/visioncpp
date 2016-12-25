799206f7ff861cc0b7448c3656bd61ecacdcb4ca
Chris Cummins <chrisc.101@gmail.com>
2016-12-25 19:18:40 +0000

    python: Update gold-standard test output

--- contrib/python/visioncpp/tests/test_codegen.py
+++ contrib/python/visioncpp/tests/test_codegen.py
@@ -22,14 +22,14 @@ class test_codegen(TestCase):
 
 extern "C" {
 
-int native_expression_tree(unsigned char *const in1, unsigned char *const out) {
+int native_expression_tree(unsigned char *const Image_1_arg, unsigned char *const out) {
 auto device = visioncpp::make_device<visioncpp::backend::sycl, visioncpp::device::cpu>();
 
 // inputs:
 std::shared_ptr<unsigned char> Image_1_data(new unsigned char[786432], [](unsigned char *d) { delete[] d; });
 
 {  // compute scope
-auto Image_1 = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(in1);
+auto Image_1 = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(Image_1_arg);
 auto Image_1_out = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(Image_1_data.get());
 auto show_1 = visioncpp::assign(Image_1_out, Image_1);
 visioncpp::execute<visioncpp::policy::Fuse, 16, 16, 8, 8>(show_1, device);
@@ -53,14 +53,14 @@ memcpy(out, Image_1_data.get(), 786432);
 
 extern "C" {
 
-int native_expression_tree(unsigned char *const in1, unsigned char *const out) {
+int native_expression_tree(unsigned char *const Image_1_arg, unsigned char *const out) {
 auto device = visioncpp::make_device<visioncpp::backend::sycl, visioncpp::device::gpu>();
 
 // inputs:
 std::shared_ptr<unsigned char> Image_1_data(new unsigned char[786432], [](unsigned char *d) { delete[] d; });
 
 {  // compute scope
-auto Image_1 = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(in1);
+auto Image_1 = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(Image_1_arg);
 auto Image_1_out = visioncpp::terminal<visioncpp::pixel::U8C3, 512, 512, visioncpp::memory_type::Buffer2D>(Image_1_data.get());
 auto show_1 = visioncpp::assign(Image_1_out, Image_1);
 visioncpp::execute<visioncpp::policy::Fuse, 16, 16, 8, 8>(show_1, device);
